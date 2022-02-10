e71629f919bc6edcfd28225233d29b5362cfdab8
Daljinder <76115323+daljinder-singh@users.noreply.github.com>
2022-02-10 09:11:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import routes from './src/router/routes.js';
+const __dirname = path.resolve();
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(routes)
+  
+const PORT = 5000;
+  
+app.listen(PORT,() => {
+    console.log(`Running on PORT ${PORT}`);
+})
